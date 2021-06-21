6f8da5dc91de208434c865d955155e2d354be409
Noah <63881698+NoahNacho@users.noreply.github.com>
2021-06-21 12:39:57 -0400

    Create Example1.py

---
+++ Chapter2/Example1.py
@@ -0,0 +1,14 @@
+#Take the sentence: All work and no play makes Jack a dull boy. Store each word in a separate variable, then print out the sentence on one line using print.
+
+var1 = "All "
+var2 = "work "
+var3 = "and "
+var4 = "no "
+var5 = "play "
+var6 = "makes "
+var7 = "Jack "
+var8 = "a "
+var9 = "dull"
+var10 = "boy."
+
+print(var1+var2+var3+var4+var5+var6+var7+var8+var9+var10)
